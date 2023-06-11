c4e21cc6ec78a22cc5f7aa18781fc19d8c63cfdc
KelyLenna <131323397+KelyLenna@users.noreply.github.com>
2023-06-11 19:32:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, "./views/index.html"))
-});
+app.get('/', (req,res)=>{
+    res.render("headerHome")
+})
+app.get('/', (req,res)=>{
+    res.render("footerHome")
+})
+app.get('/', (req,res)=>{
+    res.render("bodyHome")
+})
 
 app.listen(3000, () =>{
     console.log("Servidor corriendo en el puerto 3000")
